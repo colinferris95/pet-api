b089c7057c8f64ccd8b358456bc458eb53764967
Colin Ferris <colinferris@Colins-MacBook-Pro.local>
2021-12-29 15:05:35 -0500

    add basic auth

--- src/main/kotlin/com/colin/petapi/PetApiApplication.kt
+++ src/main/kotlin/com/colin/petapi/PetApiApplication.kt
@@ -2,10 +2,13 @@ package com.colin.petapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-
 @SpringBootApplication
 class PetApiApplication
+
+
+
 
 fun main(args: Array<String>) {
     runApplication<PetApiApplication>(*args)
 }
+
